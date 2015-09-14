97c4cf534393468dc12f12b99bc50545ce0c5d99
Andrew Bibb <ajbibb@myfairpoint.net>
2015-09-14 19:06:10 -0400

    updated with code in 2015.09.15 release

--- code/resource.h
+++ code/resource.h
@@ -32,8 +32,8 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 //
 // Program Info (may be visible, but don't mark for tranalation) 
-#define VERSION "2015.09.13-3"
-#define RELEASE_DATE "6 September 2015"
+#define VERSION "2015.09.15-1"
+#define RELEASE_DATE "15 September 2015"
 #define COPYRIGHT_DATE "2013-2015"
 
 // Program Values:
