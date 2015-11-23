b0582573fba27cd85ea426288967ad611ab8a7a4
Andrew Bibb <ajbibb@myfairpoint.net>
2015-11-22 19:23:22 -0500

    making sure remote is up to date

--- code/resource.h
+++ code/resource.h
@@ -32,7 +32,7 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 //
 // Program Info (may be visible, but don't mark for tranalation) 
-#define VERSION "2015.11.22-2"
+#define VERSION "2015.11.22-3"
 #define RELEASE_DATE "22 November 2015"
 #define COPYRIGHT_DATE "2013-2015"
 
